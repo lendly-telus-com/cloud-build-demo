2781d4b5773169414732bedbaa4fd05b308b492e
Lendly Cagata <lendly.cagata@telus.com>
2023-07-17 12:49:09 +0800

    feat: update hello functions

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-hello = () => {
-  return 'Hello Pogs!';
-};
+const functions = require('@google-cloud/functions-framework');
+
+// Register an HTTP function with the Functions Framework that will be executed
+// when you make an HTTP request to the deployed function's endpoint.
+functions.http('helloGET', (req, res) => {
+  res.send('Hello Pogs!');
+});
